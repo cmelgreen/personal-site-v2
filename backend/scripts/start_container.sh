de535ea41305a9b6758ee9698050a5b830be863d
cmelgreen <cmelgreen@gmail.com>
2021-03-03 22:42:05 -0600

    move docker prune to AfterInstall

--- backend/scripts/start_container.sh
+++ backend/scripts/start_container.sh
@@ -2,4 +2,3 @@
 
 docker pull 010629071893.dkr.ecr.us-east-1.amazonaws.com/test:latest
 docker run --name server -p 80:80 -p 443:443 -v /certs/:/certs/ -d 010629071893.dkr.ecr.us-east-1.amazonaws.com/test:latest 
-docker image prune -af
