f4de3ed69f58cf3774c28cbe59bca5984b9a4b86
cmelgreen <cmelgreen@gmail.com>
2021-03-10 00:35:33 -0600

    work on frontend redesign

--- frontend/src/Components/AboutMe.js
+++ frontend/src/Components/AboutMe.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import { forwardRef } from 'react'
 
 import { makeStyles } from "@material-ui/core/styles";
 import { Container, Typography } from '@material-ui/core'
 
 const useStyles = makeStyles((theme) => ({
   aboutMe: {
-    backgroundColor: '#353440',
+    backgroundColor: '#2C3359',
     padding: '10%',
     margin: '5% auto',
     minHeight: 200,
@@ -20,17 +21,19 @@ const useStyles = makeStyles((theme) => ({
   },
 }))
 
-export default function About(props) {
+const About = forwardRef((props, ref) => {
   const classes = useStyles()
 
   return (
-    <Container className={classes.aboutMe} maxWidth='md'>
-      <Typography className={classes.title} variant="h1" align="center">
-        About Me
-      </Typography>
-      <Typography className={classes.text} variant="subtitle1">
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-      </Typography>
+    <Container className={classes.aboutMe} maxWidth='md' ref={ref}>
+        <Typography className={classes.title} variant="h1" align="center">
+            About
+        </Typography>
+        <Typography className={classes.text} variant="subtitle1">
+            After 6 great years of Enterprise Software-as-a-Service sales, pivoting back towards the technical side of SaaS and full-stack software development
+        </Typography>
     </Container>
   )
-}
+})
+
+export default About
